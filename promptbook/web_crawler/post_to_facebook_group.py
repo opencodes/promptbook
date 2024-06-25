9abd8af8ac317a006cf7a8f9461fd9d3539b69dc
Opensource Coder <dev.opencodes@gmail.com>
2024-06-25 15:04:18 +0530

    Create post_to_facebook_group.py

---
+++ promptbook/web_crawler/post_to_facebook_group.py
@@ -0,0 +1,24 @@
+import facebook
+import requests
+
+# Facebook access token
+access_token = 'YOUR_ACCESS_TOKEN'
+
+# Facebook Group ID
+group_id = 'YOUR_GROUP_ID'
+
+# Image URL
+image_url = 'URL_OF_YOUR_IMAGE'
+
+# Initialize Facebook graph API object
+graph = facebook.GraphAPI(access_token)
+
+# Upload image to Facebook
+image = requests.get(image_url)
+data = {
+    'source': image.content,
+    'message': 'Check out this cool image!',
+}
+
+# Post image to group
+graph.put_photo(image=open('temp_image.jpg', 'rb'), album_path=f'{group_id}/photos', message='Check out this cool image!')
